de4801528ae74a71ab8dace37bbe1892654294b7
Myles <sieong@summit-tech.ca>
2018-12-10 14:53:17 -0500

    Add recipe class and testing
    Change-Id: I99dc0db1948c94c7e0f39ab0917b01066c2f4af6

--- diagnosis.kts
+++ diagnosis.kts
@@ -1,20 +1,38 @@
 import java.io.File
 
-diagnosis(args[0])
+var recipes = arrayOf(
+        Recipe(name = "Tests fail", symptoms = arrayOf(Regex("A1"), Regex("B2")), diagnosis = "Tests failed. Fix them"),
+        Recipe(name = "Test(instrumented) crash", symptoms = arrayOf(Regex("A3")), diagnosis = "Test(instrumented) crash, check the test logic"),
+        Recipe(name = "Impossible", symptoms = arrayOf(Regex("T3")), diagnosis = "Impossible")
+)
 
-fun diagnosis(filepath: String) {
+main(args[0])
 
-	var blob = readFileBlob(filepath)
-	var regex = Regex("A1((\n)*.*)*B1")
+fun main(filepath: String) {
 
-	println("blob is like:\n$blob")
-	println(if (findRegex(blob, regex)) "Match result: yes" else "Match result: no")
+    val blob = readFileBlob(filepath)
+    println("blob is like:\n$blob")
+
+    for (r in recipes){
+        println("Does it match Recipe ${r.name}? " + if (r.matches(blob)) "Yes -> ${r.diagnosis}" else "No")
+    }
 }
 
-fun findRegex(s: String, r : Regex) = r.find(s) != null
+fun String.findRegex(r : Regex): Boolean =  r.find(this) != null
 
 fun readFileBlob(path : String) : String {
-	var b = ""
-	File(path).readLines().forEach {b += "$it\n"}
-	return b
+    var b = ""
+    File(path).readLines().forEach {b += "$it\n"}
+    return b
+}
+
+/*assume all symptoms are positive*/
+class Recipe(var name: String, var symptoms: Array<Regex> , var diagnosis: String) {
+    fun matches(blob : String) : Boolean {
+        var match = true
+        for (sRegex in symptoms) {
+            match = blob.findRegex(sRegex)
+        }
+        return match
+    }
 }
