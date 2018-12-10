9649ee4972541fd42aa18e1f78290fb84f2fa7eb
Myles <sieong@summit-tech.ca>
2018-12-10 15:07:09 -0500

    Finish basic functionality
    Change-Id: Id514ed0a51ecf5b396a7cef72064fd69967e218b

--- diagnosis.kts
+++ diagnosis.kts
@@ -1,9 +1,10 @@
 import java.io.File
 
 var recipes = arrayOf(
-        Recipe(name = "Tests fail", symptoms = arrayOf(Regex("A1"), Regex("B2")), diagnosis = "Tests failed. Fix them"),
-        Recipe(name = "Test(instrumented) crash", symptoms = arrayOf(Regex("A3")), diagnosis = "Test(instrumented) crash, check the test logic"),
-        Recipe(name = "Impossible", symptoms = arrayOf(Regex("T3")), diagnosis = "Impossible")
+        Recipe(name = "Tests fail", symptoms = arrayOf("A1", "B2"), diagnosis = "Tests failed. Fix them"),
+        Recipe(name = "Tests fail (reverse)", symptoms = arrayOf("B2", "A1"), diagnosis = "Tests failed (Reverse). Fix them"),
+        Recipe(name = "Test(instrumented) crash", symptoms = arrayOf("A3"), diagnosis = "Test(instrumented) crash, check the test logic"),
+        Recipe(name = "Impossible", symptoms = arrayOf("T3"), diagnosis = "Impossible")
 )
 
 main(args[0])
@@ -11,14 +12,13 @@ main(args[0])
 fun main(filepath: String) {
 
     val blob = readFileBlob(filepath)
-    println("blob is like:\n$blob")
 
     for (r in recipes){
         println("Does it match Recipe ${r.name}? " + if (r.matches(blob)) "Yes -> ${r.diagnosis}" else "No")
     }
 }
 
-fun String.findRegex(r : Regex): Boolean =  r.find(this) != null
+fun String.findRegex(r : Regex, startIndex : Int = 0): MatchResult? =  r.find(this, startIndex)
 
 fun readFileBlob(path : String) : String {
     var b = ""
@@ -27,12 +27,13 @@ fun readFileBlob(path : String) : String {
 }
 
 /*assume all symptoms are positive*/
-class Recipe(var name: String, var symptoms: Array<Regex> , var diagnosis: String) {
+class Recipe(var name: String, var symptoms: Array<String> , var diagnosis: String) {
     fun matches(blob : String) : Boolean {
-        var match = true
-        for (sRegex in symptoms) {
-            match = blob.findRegex(sRegex)
+        val regexSeparator = "((\\n)*.*)*"
+        var generatedRegexString = ""
+        for (s in symptoms) {
+            generatedRegexString += s + regexSeparator
         }
-        return match
+        return blob.findRegex(Regex(generatedRegexString)) != null
     }
 }
