a25ec66ee756640126bdb19c437e8894a5ae1ff5
Myles <sieong@summit-tech.ca>
2018-12-10 14:02:04 -0500

    Can find symtops
    Change-Id: Ifb8e44fe7efed1325b01d56accbdad57cda127d2

--- diagnosis.kts
+++ diagnosis.kts
@@ -2,10 +2,19 @@ import java.io.File
 
 diagnosis(args[0])
 
+fun diagnosis(filepath: String) {
 
-fun diagnosis(r: String) {
-	File(r).readLines().forEach {
-		println(it)	
-	}
+	var blob = readFileBlob(filepath)
+	var regex = Regex("A1((\n)*.*)*B1")
+
+	println("blob is like:\n$blob")
+	println(if (findRegex(blob, regex)) "Match result: yes" else "Match result: no")
 }
 
+fun findRegex(s: String, r : Regex) = r.find(s) != null
+
+fun readFileBlob(path : String) : String {
+	var b = ""
+	File(path).readLines().forEach {b += "$it\n"}
+	return b
+}
